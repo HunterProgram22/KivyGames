4a6b2efaebf0e56c08d30c1270cfd06eae1b0ce2
Justin <JustinKudela@gmail.com>
2018-12-11 22:53:01 -0500

    First commit

---
+++ PaperRockScissors/main.py
@@ -0,0 +1,34 @@
+import kivy
+import win32com.client as win
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.lang.builder import Builder
+
+
+from os import listdir
+
+PATH = './kv/'
+for kv in listdir(PATH):
+    Builder.load_file(PATH + kv)
+
+class HomeScreen(GridLayout):
+
+    def hello_Hunter(self):
+        speaker = win.Dispatch("SAPI.SpVoice")
+        speaker.Speak("Hello Hunter, would you like to play a game?")
+
+    def hello_Tyler(self):
+        speaker = win.Dispatch("SAPI.SpVoice")
+        speaker.Speak("Hello Tyler, would you like to play a game?")
+
+
+class MyApp(App):
+    title = 'Paper Rock Scissors'
+
+    def build(self):
+        return HomeScreen()
+
+if __name__ == '__main__':
+    MyApp().run()
