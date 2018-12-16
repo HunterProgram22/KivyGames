8ed8c49bbd804f02b6457bce96f72996f398e93c
Justin <JustinKudela@gmail.com>
2018-12-16 16:17:09 -0500

    Creating Pong Tutorial.

--- PaperRockScissors/main.py
+++ PaperRockScissors/main.py
@@ -18,17 +18,34 @@ class HomeScreen(GridLayout):
     def hello_Hunter(self):
         speaker = win.Dispatch("SAPI.SpVoice")
         speaker.Speak("Hello Hunter, would you like to play a game?")
+        clear_widgets()
 
     def hello_Tyler(self):
         speaker = win.Dispatch("SAPI.SpVoice")
         speaker.Speak("Hello Tyler, would you like to play a game?")
+        clear_widgets()
 
+    speaker = win.Dispatch("SAPI.SPvoice")
+    speaker.Speak("Hello, is this Hunter or Tyler? Please press you name.")
+
+
+# class HunterScreen(GridLayout):
+#
+#     def paper_rock_scissors(self):
+#         pass
+#
+#     speaker = win.Dispatch("SAPI.SPvoice")
+#     speaker.Speak("Would you like to play Paper Rock Scissors," +\
+#         " or Guess a Number, or Tic Tac Toe.")
 
 class MyApp(App):
     title = 'Paper Rock Scissors'
-
+    
     def build(self):
         return HomeScreen()
+
+
+
 
 if __name__ == '__main__':
     MyApp().run()
